9ae4f230989d0bc35bc3e200eae85ad5a9ffe5a7
Pranshu Garg <pranshunikhil2007@gmail.com>
2026-07-16 23:38:45 +0530

    added

---
+++ weatherAgent.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import requests
+load_dotenv()
+
+client = OpenAI()
+
+def get_weather(city:str):
+    url=f"https://wttr.in/{city.lower()}?format=%C+%t"
+    response=requests.get(url)
+    if response.status_code==200:
+        return f"The weather in {city} is {response.text}"
+    return "Something went wrong"
+
+def main():
+    user_query=input("👉 : ")
+    response=client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role":"user","content":user_query} #role and content are predefined keys that openAI understands
+        ]
+    )
+    print(f"🤖 : {response.choices[0].message.content}")
+
+main()
