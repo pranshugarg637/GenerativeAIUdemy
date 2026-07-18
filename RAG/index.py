ae2790009929074e6b0b7466b48b894c85f588e0
Pranshu Garg <pranshunikhil2007@gmail.com>
2026-07-18 13:18:02 +0530

    RagWithLangChain

---
+++ RAG/index.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+load_dotenv()
+
+pdf_path = Path(__file__).parent / "nodejs.pdf"
+
+#loading file in python document
+loader = PyPDFLoader(file_path = pdf_path)
+docs=loader.load()
+
+# chunking / text splitting
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 400
+)
+chunks = text_splitter.split_documents(documents=docs)
+
+#vector embeddings
+embedding_model = OpenAIEmbeddings(
+    model = "text-embedding-3-large"
+)
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    url="http://localhost:6333",
+    collection_name="learning_rag"
+)
+
+print("Ingestion pipeline completed")
